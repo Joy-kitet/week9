7c6722881b9a62a47e2a3ffa3f39b985f1f7781d
Joy-kitet <debrajoykitet@gmail.com>
2025-06-12 11:00:52 +0300

    modified files

--- week3/encapsulation.dart
+++ week3/encapsulation.dart
@@ -1,22 +1,43 @@
-void main(){
-Circle circle =Circle( 5.0);
+// void main(){
+// Circle circle =Circle( 5.0);
 
-print(circle.area); // Output: 78.5
+// print(circle.area); // Output: 78.5
+
+// }
+
+
+// class Circle{
+//   double radius;
+
+//   Circle(this.radius);
+
+//   double get area => 3.14 * radius * radius;
+
+//   void setRadius(double newRadius) {
+//     if (newRadius > 0) {
+//       radius = newRadius;
+//     } else {
+//       print("Radius must be positive");
+//     }
+//   }
+// }
+
+class Person{
+  int _age=3;
+  String _name= 'Joy';
+  
+  //getter methods
+  int get age=>_age;
+  
+  
+  String get name=>name;
+  
+  
 }
 
-
-class Circle{
-  double radius;
-
-  Circle(this.radius);
-
-  double get area => 3.14 * radius * radius;
-
-  void setRadius(double newRadius) {
-    if (newRadius > 0) {
-      radius = newRadius;
-    } else {
-      print("Radius must be positive");
-    }
-  }
+void main(){
+  Person p=Person();
+  
+  print(p._age);
+  print(p._name);
 }
---
+++ week3/laptop.dart
@@ -0,0 +1,65 @@
+//create a class for Laptop
+// lets create a class called Laptop
+class Laptop {
+  // Properties of the Laptop class
+  String brand;
+  String model;
+  String processor;
+  int ram; // in GB
+  int storage; // in GB
+  String graphicsCard;
+  String operatingSystem;
+  double screenSize; // in inches
+  int batteryLife; // in hours
+  int price; // in USD
+  bool isTouchscreen;
+ 
+  // Constructor to initialize all fields //named constructor
+  Laptop({
+    required this.brand,
+    required this.model,
+    required this.processor,
+    required this.ram,
+    required this.storage,
+    required this.graphicsCard,
+    required this.operatingSystem,
+    required this.screenSize,
+    required this.batteryLife,
+    required this.isTouchscreen,
+    required this.price,
+  });
+ 
+  // Method to display laptop details
+  void displayDetails() {
+    print('Brand: $brand');
+    print('Model: $model');
+    print('Processor: $processor');
+    print('RAM: $ram GB');
+    print('Storage: $storage GB');
+    print('Graphics Card: $graphicsCard');
+    print('Operating System: $operatingSystem');
+    print('Screen Size: $screenSize inches');
+    print('Battery Life: $batteryLife hours');
+    print('Touchscreen: ${isTouchscreen ? "Yes" : "No"}');
+  }
+  void main() {
+    // Create an instance of the Laptop class
+    Laptop myLaptop = Laptop(
+      brand: 'Dell',
+      model: 'XPS 15',
+      processor: 'Intel Core i7',
+      ram: 16,
+      storage: 512,
+      graphicsCard: 'NVIDIA GeForce GTX 1650',
+      operatingSystem: 'Windows 10',
+      screenSize: 15.6,
+      batteryLife: 10,
+      isTouchscreen: false,
+      price: 1500,
+    );
+ 
+    // Display the laptop details
+    myLaptop.displayDetails();
+  }
+}
+ 
